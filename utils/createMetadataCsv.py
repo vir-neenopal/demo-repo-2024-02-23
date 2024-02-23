e27bdd17484e37ee37c06a30cef928a4335246a1
vir-neenopal <viraj.jayant@neenopal.com>
2024-02-23 19:00:36 +0530

    commit-2

--- utils/createMetadataCsv.py
+++ utils/createMetadataCsv.py
@@ -1,8 +1,9 @@
 # sample_class.py
+import pandas as pd
 
 class SampleClass:
     def __init__(self, name):
         self.name = name
 
     def greet(self):
-        return f"Hello, {self.name}!"
+        return f"Hello, {self.name}!", f"{pd.DataFrame()}"
